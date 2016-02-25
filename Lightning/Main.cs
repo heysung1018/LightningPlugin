4df0fe7adc1126503e661b3064a8854e6e871ee8
sepi <deet1022@gmail.com>
2016-02-26 03:14:10 +0900

    add file

---
+++ Lightning/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using MiNET;
+using MiNET.Net;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Utils;
+
+namespace Lightning {
+	[Plugin(Author = "sepi", Description = "번개좀 맞아봐라 지지직", PluginName = "Lightning", PluginVersion = "1.0-Alpha")]
+	public class Lightinig
+	{
+		[PacketHandler, Receive]
+		public void MessageHandler(McpeText message, Player player) {
+			string text = message.message;
+			if(text == "번개 맞고 싶다") {
+				for(int i = 0; i < 6; i++) {
+					player.StrikeLightning();
+					player.SendMessage(ChatColors.Yellow + "아하하하하 번개맛이 어떠냥!", type: MessageType.Raw);
+					Thread.Sleep(1000);
+				}
+			}	
+		}
+	}
+}
