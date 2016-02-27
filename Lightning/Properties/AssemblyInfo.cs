b242c239443a3ae0f5d8e531e515c395bce3c0b3
sepi <deet1022@gmail.com>
2016-02-27 11:20:35 +0900

    파일 수정

--- Lightning/Properties/AssemblyInfo.cs
+++ Lightning/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lightning")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  sepi, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
